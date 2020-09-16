b3a39f50145e850df22dca2b65ed45870dc50804
Kim-hyeran <hran718@naver.com>
2020-09-16 16:19:15 +0900

    NodeJS Hello 3

--- Node_Hello_3/routes/index.js
+++ Node_Hello_3/routes/index.js
@@ -13,7 +13,6 @@ router.post("/", function (req, res) {
   let m_user=req.body.m_user;
   let date=moment(new Date()).format("YYYY-MM-DD");
   let time=moment(new Date()).format("HH:mm:ss");
-  //let title = name + "님 반갑습니다";
 
   res.render("index", {title : m_user+"님 반갑습니다", date : date, time : time});
 });
